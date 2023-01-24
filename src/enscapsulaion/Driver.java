1969dd5b79a9d29a22c45f66a2822f868bc9988c
VICKY GUPTA <119840959+vickygupta415@users.noreply.github.com>
2023-01-24 21:28:57 +0530

    Add files via upload

---
+++ src/enscapsulaion/Driver.java
@@ -0,0 +1,15 @@
+package enscapsulaion;
+
+public class Driver {
+	public static void main(String[] args) {
+		Laptop l1=new Laptop();
+	l1.setName("hp");
+	l1.setBrand("leanavo");
+	l1.setPrice(4561252);
+	System.out.println(l1.getBrand());
+	System.out.println(l1.getName());
+	System.out.println(l1.getPrice());
+		
+	}
+
+}
---
+++ src/enscapsulaion/Laptop.java
@@ -0,0 +1,29 @@
+package enscapsulaion;
+
+public class Laptop {
+	private String name;
+	private int price;
+	private String brand;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	
+	
+
+}
